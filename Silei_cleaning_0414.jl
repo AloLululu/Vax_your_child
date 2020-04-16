f320c412cb7c0f9c912eb2ff33581b004c1ec82a
AloLululu <>
2020-04-15 21:46:13 -0400

    Update Silei_cleaning_0414.jl

--- Silei_cleaning_0414.jl
+++ Silei_cleaning_0414.jl
@@ -1,14 +1,14 @@
 println("I am doing basic data cleaning")# test
 
-path_kids = 
-path_teens = 
+path_kids = "C:\\Users\\lisil\\methods2020\\methods2020_vaxyourchild\\NISPUF18.DAT"
+path_teens = "C:\\Users\\lisil\\methods2020\\methods2020_vaxyourchild\\NISTEENPUF18.DAT"
 
 using CSV
 using DataFrames
 
-kids_data = CSV.file()    |>DataFrame
-teens_data = 
+#kids_data = open(path_kids) |> DataFrame
+#teens_data = 
 
-
-
-
+#readdlm(path_kids," ")
+kids_data = CSV.read(path_kids, delim = " ") => DataFrame
+describe(kids_data)
